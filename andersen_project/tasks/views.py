ad596ee5d4ccbb74a602c40f29608125f0b6165f
murad48oguz <ebulzade@inbox.ru>
2025-08-17 18:33:29 +0400

    - mark_as_completed endpoint - status filtering - my-tasks endpoint - pagination

--- andersen_project/tasks/views.py
+++ andersen_project/tasks/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
 from .permissions import IsOwner
@@ -9,10 +11,35 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         queryset = Task.objects.filter(user=self.request.user)
+        
         status_param = self.request.query_params.get("status")
         if status_param:
-            queryset = queryset.filter(status=status_param.lower())
+            status_param = status_param.lower()
+            valid_statuses = ["new", "in_progress", "completed"]
+            if status_param in valid_statuses:
+                queryset = queryset.filter(status=status_param)
+            else:
+                return Task.objects.none()
         return queryset
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+    
+    @action(detail=False, methods=['get'], url_path='my-tasks')
+    def my_tasks(self, request):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['patch'], url_path='mark-completed')
+    def mark_as_completed(self, request, pk=None):
+        task = self.get_object()
+        task.status = 'completed'
+        task.save()
+        serializer = self.get_serializer(task)
+        return Response(serializer.data)
